2dd3dc89ec7b8047f8a97a08d8332c7e7470797a
Levi Pare <levipare@gmail.com>
2024-01-26 12:12:13 -0500

    updated overloads and methods

--- date.cpp
+++ date.cpp
@@ -1,7 +1,9 @@
 #include "date.h"
 #include <ctime>
+#include <iostream>
 #include <string>
 
+// Constructors
 Date::Date() {
   std::time_t t = std::time(nullptr);
   std::tm *now = std::localtime(&t);
@@ -17,11 +19,11 @@ Date::Date(int day, int month, int year) {
 }
 
 // Getters
-int Date::get_day() { return this->day; }
+int Date::get_day() const { return this->day; }
 
-int Date::get_month() { return this->month; }
+int Date::get_month() const { return this->month; }
 
-int Date::get_year() { return this->year; }
+int Date::get_year() const { return this->year; }
 
 // Setters
 void Date::set_day(int d) { this->day = d; }
@@ -30,10 +32,35 @@ void Date::set_month(int m) { this->month = m; }
 
 void Date::set_year(int y) { this->year = y; }
 
-// Utils
-Date::WeekDay Date::week_day() { return WeekDay(static_cast<int>(this->day)); }
+// Overloaded operators
+bool Date::operator==(const Date &rhs) const {
+  return this->day == rhs.day && this->month == rhs.month &&
 
-Date::Month Date::month_name() { return Month(this->month); }
+         this->year == rhs.year;
+}
+
+std::ostream &operator<<(std::ostream &os, const Date &d) {
+  os << d.month + 1 << '/' << d.day << '/' << d.year;
+  return os;
+}
+
+std::size_t Date::operator()(const Date &d) const {
+  return std::hash<int>()(d.day) + std::hash<int>()(d.month) +
+         std::hash<int>()(d.year);
+}
+
+// Utils
+Date::WeekDay Date::week_day_enum() const {
+  std::tm tm = {};
+  tm.tm_year = this->year - 1900;
+  tm.tm_mon = this->month;
+  tm.tm_mday = this->day;
+  std::mktime(&tm);
+
+  return WeekDay(tm.tm_wday);
+}
+
+Date::Month Date::month_enum() const { return Month(this->month); }
 
 std::string week_day_to_string(Date::WeekDay d) {
   switch (d) {
