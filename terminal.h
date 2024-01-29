aac5f5155d30c6798bd1632c11988163fabca873
Levi Pare <levipare@gmail.com>
2024-01-29 11:39:23 -0500

    Documentation

--- terminal.h
+++ terminal.h
@@ -5,14 +5,31 @@
 
 namespace Terminal {
 
+/**
+ * @brief Setups signal handling.
+ */
 void setup();
 
+/**
+ * @brief Disables canonical mode.
+ */
 void set_raw();
 
+/**
+ * @brief Restores the terminal to canonical mode.
+ */
 void restore();
 
+/**
+ * @brief Get the height (# of cols) of the current terminal
+ * @return the width of the terminal
+ */
 int get_width();
 
+/**
+ * @brief Get the height (# of rows) of the current terminal
+ * @return the height of the terminal
+ */
 int get_height();
 }; // namespace Terminal
 
