02e93125fbd95f41aa89c6ef947d0032a0993b1c
Levi Pare <levipare@gmail.com>
2024-02-01 10:50:46 -0500

    removed old code

--- main.cpp
+++ main.cpp
@@ -21,17 +21,6 @@ int main() {
   std::vector<std::shared_ptr<Renderable>> render_objects = {planner, input};
   Renderer renderer(render_objects);
 
-  // TODO: Add a csv parser and generator to load/save tasks to a file
-  // Some test tasks
-  // Task t1("Do HW1 helllo world another task", Date(23, 0, 2024));
-  // Task t2("Do HW2", Date(23, 0, 2024));
-  // Task t3("Do HW3", Date(24, 0, 2024));
-  // Task t4("Do HW4", Date(1, 1, 2024));
-  // planner->add_task(std::make_shared<Task>(t1));
-  // planner->add_task(std::make_shared<Task>(t2));
-  // planner->add_task(std::make_shared<Task>(t3));
-  // planner->add_task(std::make_shared<Task>(t4));
-
   // Setup terminal enviroment
   Terminal::set_raw();
   bool quit = false;
